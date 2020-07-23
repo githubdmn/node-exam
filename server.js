3eb0d6c9dad127ef5f7846dc30cf1601aa1f9f9b
githubdmn <dmninbox@gmail.com>
2020-07-23 15:40:19 +0200

    exam template

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const PORT = 8000;
+
+const http = require('http');
+const string_decoder = require('string_decoder');
+const url = require('url');
+
+http
+    .createServer((req, res) => {
+        try {
+            const parsedUrl = url.parse(req.on, true);
+            const urlPath = parsedUrl.pathname;
+            const trimmedPath = urlPath.replace(/^\/+|\/+$/g, '');
+            if (trimmedPath === 'codility-demo') {
+
+            } else {
+                throw 'Path is not correct';
+            }
+        } catch (exception) {
+            console.error(exception);
+            res.end(exception);
+        }
+
+    })
+    .listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    })
+
+function task01() {
+
+}
